df7168d62802c255bb1652414b3d197288a6b3ba
Jorge Jimenez <xadrijo@gmail.com>
2017-10-18 12:42:19 -0300

    Improving code

--- app/Repositories/Facebook.php
+++ app/Repositories/Facebook.php
@@ -6,24 +6,20 @@ use GuzzleHttp\Client;
 
 class Facebook
 {
+	private $token = 'EAACEdEose0cBADlSLnkWlxmZA7G6mU8uVKd9uscM1OpCxZB7y4rKZBx3WM7JtibQjvAW97cOyhu9FnanDVAnBwpR0u9GcSxpe44qyDCzsjuxx6ZAgU3bBVrdYeNEfMXMrgkUegdmYYJwRZBuwbyc94gBICPdENLoyry7YY7tGFkqTNd7i8GIKjZAv5uDjSdRoZD';
+
 	public function __construct(Client $client)
 	{
-		$this->client = $client;
+		$this->client = new Client([
+		    'base_uri' => 'https://graph.facebook.com/',
+		]);
 	}
 
 	public function user($id) 
 	{
-		$client = new Client([
-		    'base_uri' => 'https://graph.facebook.com/',
+		$response = $this->client->request('GET', $id . '?access_token=' . $this->token);
 
-		    'timeout'  => 2.0,
-		]);
-
-		$token = 'EAACEdEose0cBADlSLnkWlxmZA7G6mU8uVKd9uscM1OpCxZB7y4rKZBx3WM7JtibQjvAW97cOyhu9FnanDVAnBwpR0u9GcSxpe44qyDCzsjuxx6ZAgU3bBVrdYeNEfMXMrgkUegdmYYJwRZBuwbyc94gBICPdENLoyry7YY7tGFkqTNd7i8GIKjZAv5uDjSdRoZD';
-
-		$response = $client->request('GET', $id . '?access_token=' . $token);
-
-		return $response->getBody();		
+		return $response->getBody();
 	}
 
 }
