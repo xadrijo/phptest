4ab186bc4d78402165600bec684595c8466ef846
Jorge Jimenez <xadrijo@gmail.com>
2017-10-18 12:03:00 -0300

    Added controller and show method

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class FacebookController extends Controller
@@ -9,6 +10,16 @@ class FacebookController extends Controller
 	// Show profile information
     public function show($id)
     {
-    	return $id;
+    	$client = new Client([
+		    'base_uri' => 'https://graph.facebook.com/',
+
+		    'timeout'  => 2.0,
+		]);
+
+		$token = 'EAACEdEose0cBADlSLnkWlxmZA7G6mU8uVKd9uscM1OpCxZB7y4rKZBx3WM7JtibQjvAW97cOyhu9FnanDVAnBwpR0u9GcSxpe44qyDCzsjuxx6ZAgU3bBVrdYeNEfMXMrgkUegdmYYJwRZBuwbyc94gBICPdENLoyry7YY7tGFkqTNd7i8GIKjZAv5uDjSdRoZD';
+
+		$response = $client->request('GET', $id . '?access_token=' . $token);
+
+		return $response->getBody();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,4 @@
 <?php
 
-use GuzzleHttp\Client;
+Route::get('profile/facebook/{id}', 'FacebookController@show');
 
-Route::get('/{id}', function($id) {
-    
-    $client = new Client([
-	    'base_uri' => 'https://graph.facebook.com/',
-
-	    'timeout'  => 2.0,
-	]);
-
-	$token = 'EAACEdEose0cBADlSLnkWlxmZA7G6mU8uVKd9uscM1OpCxZB7y4rKZBx3WM7JtibQjvAW97cOyhu9FnanDVAnBwpR0u9GcSxpe44qyDCzsjuxx6ZAgU3bBVrdYeNEfMXMrgkUegdmYYJwRZBuwbyc94gBICPdENLoyry7YY7tGFkqTNd7i8GIKjZAv5uDjSdRoZD';
-
-	$response = $client->request('GET', $id . '?access_token=' . $token);
-	return $response->getBody();
-
-});
